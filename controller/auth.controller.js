2bb872a60d438a3ff388daa5bd28e060c5cb49f1
thien <phucdt@d-soft.com.vn>
2020-08-30 00:04:38 +0700

    fix token

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -29,7 +29,7 @@ module.exports.login = function(req, res) {
         // if user is found and password is valid
         // create a token
         var token = jwt.sign({ id: user._id, role: user.role }, process.env.secret, {
-            expiresIn: 3600 // expires in 1 hours
+            expiresIn: 36000000 // expires in 1 hours
         });
 
         // return the information including token as JSON
