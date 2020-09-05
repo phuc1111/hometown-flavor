cbe671f8280e3fa98ee2ecdc20e2b1a28ba11dbf
thien <phucdt@d-soft.com.vn>
2020-09-05 18:06:24 +0700

    fix date

--- autoCreate/date.js
+++ autoCreate/date.js
@@ -3,12 +3,12 @@ module.exports.getCurrentDay = function () {
     var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
     var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     var dateTime = date;
-    return dateTime;
+    return today;
 }
 module.exports.getNextDay = function () {
     var today = new Date();
     var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + (today.getDate() + 1);
     // var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     var dateTime = date;
-    return dateTime;
+    return today;
 }
